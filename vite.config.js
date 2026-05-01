b5364d1fd556e496c6f3deeaea6a1733c813f579
muh718 <malgarni0212s3@gmail.com>
2026-05-01 23:03:35 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // نترك base فارغة أو نستخدم './' لضمان عمل المسارات بشكل صحيح عند النشر
-  base: '', 
+  base: '', // ضروري لعمل الروابط في Vercel
 })
 
 ```
+
 
