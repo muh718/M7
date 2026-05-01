f126beb103711f392196352970a9a517f1065496
muh718 <malgarni0212s3@gmail.com>
2026-05-01 23:20:15 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
-```react
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+```javascript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default defineConfig({
+  plugins: [react()],
+  base: ''
+})
 
 ```
