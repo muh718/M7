9f2b1ecb4bc4f1318d8e03a593f4aee45cca6198
muh718 <malgarni0212s3@gmail.com>
2026-05-01 17:33:47 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,13 @@
+```javascript
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // نترك base فارغة أو نستخدم './' لضمان عمل المسارات بشكل صحيح عند النشر
+  base: '', 
+})
+
+```
 
